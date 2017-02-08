ef2dc4385576519a92ea4fd48757cf875dd67221
008 MD313- <md313-008@md313-008.local>
2017-02-08 10:48:41 +0900

    ghg

--- slidetest1/ViewController.swift
+++ slidetest1/ViewController.swift
@@ -17,8 +17,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        open.target = self.revealViewController()
-        open.action = Selector("revealToggle:")
+        //open.target = self.revealViewController()
+       // open.action = Selector("revealToggle:")
         
         
     }
