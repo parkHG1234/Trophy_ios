64678e5f7ea7fbfb2561ae18fb04bad62ec0d0b8
ldong <md313-004@md313-004.local>
2017-03-01 20:05:55 +0900

    합시다

---
+++ slidetest1/ChangePhoneViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChangePhoneViewController.swift
+//  slidetest1
+//
+//  Created by ldong on 2017. 3. 1..
+//  Copyright © 2017년 MD313-008. All rights reserved.
+//
+
+import UIKit
+
+class ChangePhoneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
